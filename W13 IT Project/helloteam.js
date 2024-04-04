1aba7d95783ea292b9cef7cb651cd3222880feec
Makai-Maetani <102011493+Makai-Maetani@users.noreply.github.com>
2024-04-04 17:33:12 -0600

    Add files via upload

---
+++ W13 IT Project/helloteam.js
@@ -0,0 +1,30 @@
+const myName = "team";
+const hello = "Hello";
+const world = "World";
+const hw = "Hello World";
+const someObject = { str: "Some text", id: 5 };
+
+
+/*
+const teamM = [];
+teamM.push("Makai", "Jesse", "Logan");
+// Jesse contribution I added my name - yessir
+// Logan contribution - added my name
+console.log(hello, teamM); 
+
+//console.log(hello , myName);
+
+*/
+
+let text = "";
+const teamM = ["Makai", "Jesse", "Logan"];
+teamM.forEach(myFunction);
+
+//document.getElementById("demo").innerHTML = text;
+console.log(text)
+ 
+function myFunction(item, index) {
+  text += index + ": " + item + " "; 
+}
+
+
